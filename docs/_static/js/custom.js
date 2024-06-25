a171376a348021f3b31ccb5f266ee8cf190b4e4c
Alvaro J. Castro Rivadeneira <56451982+micokoch@users.noreply.github.com>
2024-06-25 18:58:59 -0400

    Adding Statcounter

---
+++ docs/_static/js/custom.js
@@ -0,0 +1,15 @@
+// _static/custom.js
+
+// Default Statcounter code for hubverse https://hubverse.io/
+var sc_project = 13009474;
+var sc_invisible = 1;
+var sc_security = "3d034fb6";
+
+(function() {
+    var sc = document.createElement('script');
+    sc.type = 'text/javascript';
+    sc.async = true;
+    sc.src = 'https://www.statcounter.com/counter/counter.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(sc, s);
+})();
