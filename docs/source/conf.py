bee289bb8f1692a405d935a705b7f3ca83e4155d
LucieContamin <CONTAMIN@pitt.edu>
2025-06-12 11:06:38 -0400

    update branch information

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -90,7 +90,7 @@ epub_show_urls = "footnote"
 schema_version = "v5.1.0"
 # Use schema_branch variable to specify a branch in the schemas repository from which config schema will be source, especially for docson widgets.
 # Useful if the schema being documented hasn't been released to the `main` branch in the schemas repo yet. If version has been released already, set this to "main".
-schema_branch = "br-" + schema_version
+schema_branch = "main" #"br-" + schema_version
 
 # The following statements override any custom branch assigned to schema branch if the build is being run on READTHEDOCS and is either a build for a new tag or on a branch
 # (in contrast to being run in a pull request or locally). This ensures that any production versions of the docs published on the hubDocs `main` branch always
